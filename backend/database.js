481fc8d2b9770402510a026d8d60e3b60b7ea658
Ron SG <46434857+RonSG96@users.noreply.github.com>
2024-08-13 11:40:28 -0500

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -1,10 +1,9 @@
 const { Sequelize, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
 
-// Crear una nueva instancia de Sequelize
-const sequelize = new Sequelize('suscripcion', 'admin', 'dorian2024', {
-  host: 'localhost',
-  port: 5433,
+// Crear una nueva instancia de Sequelize usando las variables de entorno
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 5432,
   dialect: 'postgres',
 });
 
