c09342922c38b60b7083e075afed211f6f02a5be
RonSG <gonzalo2196@icloud.com>
2024-09-18 12:49:52 -0500

    creacion de usuarios admin

--- backend/initializeAdmin.js
+++ backend/initializeAdmin.js
@@ -1,23 +1,37 @@
 const { Admin } = require('./database');
 const bcrypt = require('bcrypt');
 
-const initializeAdmin = async () => {
+const adminUsers = [
+  { username: 'counter-cebollar-admin', password: 'doriancebollar24' },
+  { username: 'counter-gonzalez-admin', password: 'doriangonzalez24' },
+  { username: 'counter-pi-admin', password: 'dorianpi2024' },
+  { username: 'gimnasios-dorian', password: 'Gonza2024*..' },
+];
+
+const initializeAdmins = async () => {
   try {
-    const existingAdmin = await Admin.findOne({ where: { username: 'admin' } });
-    if (!existingAdmin) {
-      const hashedPassword = await bcrypt.hash('adminpassword', 10);
-      await Admin.create({ username: 'admin', password: hashedPassword });
-      console.log('Admin user created');
-    } else {
-      console.log('Admin user already exists');
+    for (let user of adminUsers) {
+      const existingAdmin = await Admin.findOne({
+        where: { username: user.username },
+      });
+      if (!existingAdmin) {
+        const hashedPassword = await bcrypt.hash(user.password, 10);
+        await Admin.create({
+          username: user.username,
+          password: hashedPassword,
+        });
+        console.log(`Admin user ${user.username} created`);
+      } else {
+        console.log(`Admin user ${user.username} already exists`);
+      }
     }
   } catch (error) {
-    console.error('Error initializing admin:', error);
+    console.error('Error initializing admins:', error);
   }
 };
 
 const initDb = async () => {
-  await initializeAdmin();
+  await initializeAdmins();
 };
 
 initDb();
