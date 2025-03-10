61792a508de2902e50b1f94b87b437d38cadc3c9
Ron SG <46434857+RonSG96@users.noreply.github.com>
2025-03-10 10:14:50 -0500

    Update Actualizacion.js

--- frontend/src/Actualizacion.js
+++ frontend/src/Actualizacion.js
@@ -29,7 +29,7 @@ const Actualizacion = () => {
 
   const buscarUsuario = async () => {
     try {
-      const response = await fetch(`/api/actualizacion/${cedula}`);
+    const response = await fetch(`https://contratos-backend.onrender.com/api/actualizacion/${cedula}`);
       
       if (!response.ok) {
         throw new Error('Usuario no encontrado');
