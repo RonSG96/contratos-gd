263b1794ba1b1cb4a6e1cba81a03a359ba935f9c
Ron SG <46434857+RonSG96@users.noreply.github.com>
2025-03-13 18:14:49 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,15 +27,15 @@ app.use(
 
 const verifyToken = (req, res, next) => {
   const token = req.headers['x-access-token'] || req.query.token;
+  console.log('Token recibido en el servidor:', token); // Depuración
   if (!token) {
     return res.status(403).send({ auth: false, message: 'No token provided.' });
   }
 
   jwt.verify(token, SECRET_KEY, (err, decoded) => {
     if (err) {
-      return res
-        .status(500)
-        .send({ auth: false, message: 'Failed to authenticate token.' });
+      console.log('Error al verificar token:', err); // Depuración
+      return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
     }
     req.userId = decoded.id;
     next();
